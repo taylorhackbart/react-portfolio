7b3fc47dddb809bc0d40e60ac99981ac6a40b0f3
Taylor <taylor.hackbart@du.edu>
2020-12-01 17:14:13 -0800

    troubleshooting or just trouble

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const routes = require("./routes/api");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,7 +13,9 @@ app.use(express.json());
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static("client/build"));
 // }
-
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/index.html"));
+});
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/portfolio"), 
 { useNewUrlParser: true,
